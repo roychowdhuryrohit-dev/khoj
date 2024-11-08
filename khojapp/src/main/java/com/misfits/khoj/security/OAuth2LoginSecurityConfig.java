9465885e637c31d2ab11735ccb1128e61dd192a0
Harsh Saple <42319050+harshsaple@users.noreply.github.com>
2024-11-07 17:14:47 -0800

    JWT Changes

--- khojapp/src/main/java/com/misfits/khoj/security/OAuth2LoginSecurityConfig.java
+++ khojapp/src/main/java/com/misfits/khoj/security/OAuth2LoginSecurityConfig.java
@@ -3,21 +3,15 @@ package com.misfits.khoj.security;
 import static com.misfits.khoj.constants.ApplicationConstants.LOGIN_PAGE;
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class OAuth2LoginSecurityConfig {
-
-  @Value("${spring.security.oauth2.client.provider.cognito.jwt-uri}")
-  String JwtURI;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,10 +22,5 @@ public class OAuth2LoginSecurityConfig {
         .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
 
     return http.build();
-  }
-
-  @Bean
-  public JwtDecoder jwtDecoder() {
-    return NimbusJwtDecoder.withJwkSetUri(JwtURI).build();
   }
 }
