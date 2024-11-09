a6350b9695f0400b927c2f17544fdaee577c28d5
Harsh Saple <42319050+harshsaple@users.noreply.github.com>
2024-11-08 16:35:21 -0800

    Refine Exception Handling

--- khojapp/src/main/java/com/misfits/khoj/exceptions/fileexceptions/FileListingException.java
+++ khojapp/src/main/java/com/misfits/khoj/exceptions/fileexceptions/FileListingException.java
@@ -1,7 +1,26 @@
 package com.misfits.khoj.exceptions.fileexceptions;
 
+import static com.misfits.khoj.constants.ApplicationConstants.FILE_LISTING_ERROR;
+
 public class FileListingException extends FileExceptions {
+
   public FileListingException(String message, Throwable cause) {
-    super(message, "FILE_DOWNLOAD_ERROR", cause);
+    super(message, FILE_LISTING_ERROR, cause);
+  }
+
+  public FileListingException(String message) {
+    super(message, FILE_LISTING_ERROR);
+  }
+
+  public FileListingException(Throwable cause) {
+    super(cause, FILE_LISTING_ERROR);
+  }
+
+  public FileListingException() {
+    super(FILE_LISTING_ERROR);
+  }
+
+  public FileListingException(String message, FileListingException e) {
+    super(message, FILE_LISTING_ERROR, e);
   }
 }
--- khojapp/src/main/java/com/misfits/khoj/utils/KhojUtils.java
+++ khojapp/src/main/java/com/misfits/khoj/utils/KhojUtils.java
@@ -4,15 +4,32 @@ import static com.misfits.khoj.constants.ApplicationConstants.SPACE_REGEX;
 import static com.misfits.khoj.constants.ApplicationConstants.UNDERSCORE;
 
 import com.misfits.khoj.exceptions.fileexceptions.FileStandardizationException;
+import com.misfits.khoj.exceptions.userexceptions.MissingUserAttributeException;
+import com.misfits.khoj.exceptions.userexceptions.UserProfileException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KhojUtils {
 
   public static String standardizeFileName(String fileName) {
-    if (fileName == null) {
-      throw new FileStandardizationException("File name cannot be null");
+    try {
+      if (fileName == null) {
+        throw new FileStandardizationException("File name cannot be null");
+      }
+      return fileName.replaceAll(SPACE_REGEX, UNDERSCORE); // Replace spaces with underscores
+    } catch (FileStandardizationException e) {
+      throw new FileStandardizationException("File name cannot be null", e);
+    } // Replace spaces with underscores
+  }
+
+  public static String validateUserIdNotNull(String userId) {
+    try {
+      if (userId == null) {
+        throw new MissingUserAttributeException("User ID supplied cannot be null");
+      }
+      return userId;
+    } catch (UserProfileException e) {
+      throw new MissingUserAttributeException("User ID supplied cannot be null", e);
     }
-    return fileName.replaceAll(SPACE_REGEX, UNDERSCORE); // Replace spaces with underscores
   }
 }
