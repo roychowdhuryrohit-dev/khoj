91e070ef85d84e3b38be5208a568f03d87faf71c
Harsh Saple <42319050+harshsaple@users.noreply.github.com>
2024-11-20 12:57:45 -0800

    Update ChatSessionRequest.java

--- khojapp/src/main/java/com/misfits/khoj/model/chat/ChatSessionRequest.java
+++ khojapp/src/main/java/com/misfits/khoj/model/chat/ChatSessionRequest.java
@@ -1,8 +1,11 @@
 package com.misfits.khoj.model.chat;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class ChatSessionRequest {
+
+  @JsonProperty("filenames")
   private List<String> filenames;
 
   public List<String> getFilenames() {
